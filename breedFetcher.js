71dfe67861f403b477dbf7b15ec560a2cc13f5a4
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2020-11-05 23:35:42 +0000

    Refactored the app and implement testing

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,38 +1,29 @@
 // External modules and aliases
 const request = require('request');
-const { log } = console;
 
-// ------------- CLI-input -------------
-const breed = process.argv[2];
-  
-
-// ------ Breed info handler--- ----
-const callbackPrintBreedDesc = function(data) {
-  if (data === '[]') {
-    log('Breed not found!');
-  } else {
-    log(JSON.parse(data)[0].description);
-  }
-  process.exit();
-};
 
 // ----- Try to request a breed's info ---------
-const reqURL = function(breed, callback) {
+const fetchBreedDescription = function(breed, callback) {
   const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + breed;
 
   request(url, (error, response, body) => {
 
     if (response && response.statusCode === 200) {
-      callback(body);
+      
+      //let description = '';
+      if (body === '[]') {
+        callback('Breed not found!', null);
+        process.exit();
+      }
+      const { description } = JSON.parse(body)[0];
+      callback(error, description);
 
     } else if (!response) {
-      console.log('Request failed! Try again later.');
-      process.exit();
+      callback(error);
     } else {
-      console.log(`Error ${response.statusCode} '${response.statusMessage}'`);
-      process.exit();
+      callback(`${response.statusCode} '${response.statusMessage}'`);
     }
   });
 };
 
-reqURL(breed, callbackPrintBreedDesc);
+module.exports = { fetchBreedDescription };
